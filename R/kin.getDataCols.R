c626be2d1781c21f9994ff5e822d39cbc0e7901a
Carlo Campagnoli <carlo.campagnoli@gmail.com>
2023-07-30 20:54:30 +0100

    kin.getDataCols now shows default and current five mandatory columns names

--- R/kin.getDataCols.R
+++ R/kin.getDataCols.R
@@ -1,5 +1,13 @@
 #' @export
 kin.getDataCols <- function()
 {
-  get("dataCols", kinesis_parameters)
+  output <- as.data.frame(
+  matrix(c("subjName","frameN","time","deltaTime","trialN",
+         get("dataCols", kinesis_parameters)), nrow = 2, byrow = T)
+  )
+
+  colnames(output) <- NULL
+  rownames(output) <- c("DEFAULT","CURRENT")
+
+  return(output)
 }
